bba5f51894bf62bafc311782e5181e014ee8fb17
unknown <saurabh.s@spintly.com>
2022-11-08 18:57:32 +0530

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=d947c340733f4a81b51631ff1641f91f&query=' + latitude + ',' + longitude 
+
+    request({ url, json: true}, (error, { body } = {} ) => {
+        if (error) {
+            callback('Unable to connect to weather sevice!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location.', undefined)
+        } else {
+            callback(undefined, body.current.weather_descriptions[0] + '. The current temperature is ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees.')
+        }
+    })
+}
+
+module.exports = forecast
